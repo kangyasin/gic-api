c4fe5754a2a5d89a2e7cf78213d84710b3369e9a
kangyasin <tutorialblogmarine@gmail.com>
2021-03-19 16:08:04 +0700

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,17 @@
 @extends('template')
 @section('title', 'Home')
 @section('content')
-    Test
+    <div class="row mt-4">
+        <h2>GIC TEST : API dan WEB</h2>
+        <p>
+            Aplikasi ini untuk kebutuhan test GIC API Crud dan WEB Crud, repository api, web dan mobile apps kotlin ini tersedia di github @kangyasin :
+        </p>
+
+        <ul>
+            <li><a href="https://github.com/kangyasin/gic-api" target="_blank">GIT Api</a></li>
+            <li><a href="https://github.com/kangyasin/gic-mobile" target="_blank">GIT Mobile</a></li>
+            <li><a href="https://github.com/kangyasin/gic-api/blob/main/README.md" target="_blank">Readme Installation</a></li>
+
+        </ul>
+    </div>
 @endsection
